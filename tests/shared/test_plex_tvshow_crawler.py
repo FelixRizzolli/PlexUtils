c0519def98830ef8ed837c8eea26f4c97a2284fa
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-02 20:56:28 +0200

    add test_crawl_get_tvshows_count

---
+++ tests/shared/test_plex_tvshow_crawler.py
@@ -0,0 +1,38 @@
+import unittest
+import os
+
+from plex_tvshow_crawler import PlexTvshowCrawler
+
+current_script_dir = os.path.dirname(os.path.realpath(__file__))
+data_dir = os.path.join(current_script_dir, '../../data')
+tvshows_dir = os.path.join(data_dir, 'tvshows')
+
+class TestPlexTvshowsCrawler(unittest.TestCase):
+
+    def setUp(self):
+        self.tvshow_directories = [
+            "Game of Thrones (2011) {tvdb-121361}",
+            "Money Heist (2017) {tvdb-327417}",
+            "My Name (2021) {tvdb-397441}",
+            "Code Geass () {tvdb-79525}",
+        ]
+
+        if not os.path.isdir(data_dir):
+            os.mkdir(data_dir)
+        if not os.path.isdir(tvshows_dir):
+            os.mkdir(tvshows_dir)
+
+        for tvshow_directory in self.tvshow_directories:
+            if not os.path.isdir(os.path.join(tvshows_dir, tvshow_directory)):
+                os.mkdir(os.path.join(tvshows_dir, tvshow_directory))
+
+        self.crawler = PlexTvshowCrawler(tvshows_dir)
+        self.crawler.crawl()
+
+    def test_crawl_get_tvshows_count(self):
+        tvshows = self.crawler.get_tvshows()
+        self.assertEqual(len(self.tvshow_directories), len(tvshows))
+
+
+if __name__ == '__main__':
+    unittest.main()
