1725c7ad40b551044bf8ffc1da126721cf880265
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-02 20:56:01 +0200

    add PlexTvshowCrawler

---
+++ src/shared/plex_tvshow_crawler.py
@@ -0,0 +1,28 @@
+class PlexTvshowCrawler(object):
+    def __init__(self, path):
+        self.invalid_tvshows = None
+        self.invalid_seasons = None
+        self.invalid_episodes = None
+        self.tvshows = None
+        self.path = path
+
+    def crawl(self):
+        self.tvshows = []
+
+    def get_tvshows(self):
+        return self.tvshows
+
+    def get_seasons(self, tvshow_id):
+        return []
+
+    def get_episodes(self, tvshow_id, season_id):
+        return []
+
+    def get_invalid_tvshows(self):
+        return self.invalid_tvshows
+
+    def get_invalid_seasons(self):
+        return self.invalid_seasons
+
+    def get_invalid_episodes(self):
+        return self.invalid_episodes
