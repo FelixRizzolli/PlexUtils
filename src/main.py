5f0bbfe56175eb894f78fd660fa2743026119637
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-02 18:01:51 +0200

    add config yaml file

--- src/main.py
+++ src/main.py
@@ -1,15 +1,26 @@
 from shared.menu import Menu
 import gettext
 import os
+import yaml
+
+# Get the current script directory
+current_script_dir = os.path.dirname(os.path.realpath(__file__))
+
+def load_config(config_file):
+    with open(config_file, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
+
 
 def setup_i18n():
-    # Get the current script directory
-    current_script_dir = os.path.dirname(os.path.realpath(__file__))
-
     # Construct the path to the locale directory
     locale_dir = os.path.join(os.path.dirname(current_script_dir), 'locale')
 
-    trans = gettext.translation("plexutils", locale_dir, ["de"])
+    language = "en_US"
+    if 'language' in config:
+        language = config['language']
+
+    trans = gettext.translation('plexutils', locale_dir, [language], fallback=True)
     trans.install()
     return trans.gettext
 
@@ -37,5 +48,7 @@ def menu():
 
 
 if __name__ == '__main__':
+    config_dir = os.path.join(os.path.dirname(current_script_dir) + '/config.yaml')
+    config = load_config(config_dir)
     _ = setup_i18n()
     menu()
