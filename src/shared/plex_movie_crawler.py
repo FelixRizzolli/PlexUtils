b9456ebed5e6b0dd8e8672072cfcf7069ad0976c
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-02 20:02:11 +0200

    add PlexMovieCrawler

---
+++ src/shared/plex_movie_crawler.py
@@ -0,0 +1,9 @@
+class PlexMovieCrawler(object):
+    def __init__(self, path):
+        self.path = path
+
+    def crawl(self):
+        self.movies = []
+
+    def get_movies(self):
+        return self.movies
---
+++ tests/shared/test_plex_movie_crawler.py
@@ -0,0 +1,39 @@
+import unittest
+import os
+
+from plex_movie_crawler import PlexMovieCrawler
+
+current_script_dir = os.path.dirname(os.path.realpath(__file__))
+data_dir = os.path.join(current_script_dir, '../../data')
+movies_dir = os.path.join(data_dir, 'movies')
+
+class TestPlexMovieCrawler(unittest.TestCase):
+
+    def setUp(self):
+        self.movie_files = [
+            "The Matrix (1999) {tvdb-169}.mp4",
+            "The Matrix Reloaded (2003) {tvdb-553}.mp4",
+            "The Matrix Revolutions (2003) {tvdb-687}.mp4",
+            "The Matrix Resurrections (2021) {tvdb-131590}.mp4",
+            "Happy Death Day (2017) {tvdb-475}.mp4"
+        ]
+
+        if not os.path.isdir(data_dir):
+            os.mkdir(data_dir)
+        if not os.path.isdir(movies_dir):
+            os.mkdir(movies_dir)
+
+        for movie_file in self.movie_files:
+            open(os.path.join(movies_dir, movie_file), 'a').close()
+
+        self.crawler = PlexMovieCrawler(movies_dir)
+
+    def test_crawl_movies(self):
+        self.crawler.crawl()
+        movies = self.crawler.get_movies()
+
+        self.assertEqual(self.movie_files.count(), movies.count())
+
+
+if __name__ == '__main__':
+    unittest.main()
