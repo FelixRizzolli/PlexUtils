8041752c0289a8e1c7cf166d7351f9aa53b04be7
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-02 20:37:14 +0200

    write invalid movie filenames to invalid_movies in PlexMovieCrawler

--- src/shared/plex_movie_crawler.py
+++ src/shared/plex_movie_crawler.py
@@ -6,12 +6,16 @@ tvdb_id_pattern = r'{tvdb-(\d+)}'
 
 class PlexMovieCrawler(object):
     def __init__(self, path):
+        self.invalid_movies = None
         self.movies = None
         self.path = path
 
     def crawl(self):
         files = os.listdir(self.path)
+
         self.movies = []
+        self.invalid_movies = []
+
         for file in files:
             match = re.search(tvdb_id_pattern, file)
             if match:
@@ -20,9 +24,12 @@ class PlexMovieCrawler(object):
                     "filename": file,
                 }
                 self.movies.append(movie)
+            else:
+                self.invalid_movies.append(file)
+
 
     def get_movies(self):
         return self.movies
 
     def get_invalid_movies(self):
-        return []
+        return self.invalid_movies
