c3ab90d54fcf31ef4ccf70fbb756b9d0e9c2fe66
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-02 20:31:19 +0200

    extract id from movie filename

--- src/shared/plex_movie_crawler.py
+++ src/shared/plex_movie_crawler.py
@@ -1,4 +1,8 @@
+import json
 import os
+import re
+
+tvdb_id_pattern = r'{tvdb-(\d+)}'
 
 class PlexMovieCrawler(object):
     def __init__(self, path):
@@ -7,7 +11,15 @@ class PlexMovieCrawler(object):
 
     def crawl(self):
         files = os.listdir(self.path)
-        self.movies = files
+        self.movies = []
+        for file in files:
+            match = re.search(tvdb_id_pattern, file)
+            if match:
+                movie = {
+                    "id": match.group(1),
+                    "filename": file,
+                }
+                self.movies.append(movie)
 
     def get_movies(self):
         return self.movies
