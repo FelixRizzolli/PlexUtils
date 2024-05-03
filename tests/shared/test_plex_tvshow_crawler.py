ffb7952447f063755865983042b03b7e342423bb
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-03 05:44:09 +0200

    add test_crawl_get_tvshows_object

--- tests/shared/test_plex_tvshow_crawler.py
+++ tests/shared/test_plex_tvshow_crawler.py
@@ -14,7 +14,7 @@ class TestPlexTvshowsCrawler(unittest.TestCase):
             "Game of Thrones (2011) {tvdb-121361}",
             "Money Heist (2017) {tvdb-327417}",
             "My Name (2021) {tvdb-397441}",
-            "Code Geass () {tvdb-79525}",
+            "Code Geass (2006) {tvdb-79525}",
         ]
 
         if not os.path.isdir(data_dir):
@@ -32,6 +32,12 @@ class TestPlexTvshowsCrawler(unittest.TestCase):
     def test_crawl_get_tvshows_count(self):
         tvshows = self.crawler.get_tvshows()
         self.assertEqual(len(self.tvshow_directories), len(tvshows))
+
+    def test_crawl_get_tvshows_object(self):
+        tvshows = self.crawler.get_tvshows()
+        codegeass = list(filter(lambda tvshow: tvshow["dirname"] == "Code Geass (2006) {tvdb-79525}", tvshows))[0]
+        self.assertEqual(codegeass["dirname"], "Code Geass (2006) {tvdb-79525}")
+        self.assertEqual(codegeass["id"], "79525")
 
 
 if __name__ == '__main__':
