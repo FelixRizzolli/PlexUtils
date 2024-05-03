6c6bebbb49f2bb3b4894091136b2a805d181a3d5
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-03 18:41:08 +0200

    code refactoring

--- src/shared/plex_tvshow_crawler.py
+++ src/shared/plex_tvshow_crawler.py
@@ -1,84 +1,54 @@
 import json
 import os
-import re
 
-tvdb_id_pattern = r'{tvdb-(\d+)}'
-season_id_pattern = r'season (\d+)'
-episode_id_pattern = r'- s(\d+)e(\d+)'
-
-
-def crawl_seasons(tvshow_directory):
-    season_directories = os.listdir(tvshow_directory)
-
-    seasons = []
-
-    for season_dirname in season_directories:
-        match = re.search(season_id_pattern, season_dirname)
-        if match:
-            season = {
-                "id": int(match.group(1)),
-                "dirname": season_dirname,
-                "episodes": crawl_episodes(os.path.join(tvshow_directory, season_dirname))
-            }
-            seasons.append(season)
-
-    return seasons
-
-
-def crawl_episodes(directory):
-    episode_directories = os.listdir(directory)
-
-    episodes = []
-
-    for episode_filename in episode_directories:
-        match = re.search(episode_id_pattern, episode_filename)
-        if match:
-            episode = {
-                "id": int(match.group(2)),
-                "filename": episode_filename,
-            }
-            episodes.append(episode)
-
-    return episodes
-
+from tvshow_list import TVShowList
+from tvshow import TVShow
+from tvshow_season import TVShowSeason
+from tvshow_episode import TVShowEpisode
 
 class PlexTvshowCrawler(object):
     def __init__(self, path):
         self.invalid_tvshows = None
         self.invalid_seasons = None
         self.invalid_episodes = None
-        self.tvshows = None
+        self.tvshowlist = TVShowList()
         self.path = path
 
     def crawl(self):
-        directories = os.listdir(self.path)
+        tvshow_directories = os.listdir(self.path)
 
-        self.tvshows = []
         self.invalid_tvshows = []
+        self.invalid_seasons = []
+        self.invalid_episodes = []
 
-        for directory in directories:
-            match = re.search(tvdb_id_pattern, directory)
-            if match:
-                tvshow = {
-                    "id": match.group(1),
-                    "dirname": directory,
-                    "seasons": crawl_seasons(os.path.join(self.path, directory))
-                }
-                self.tvshows.append(tvshow)
+        for tvshow_dir in tvshow_directories:
+            tvshow = TVShow(tvshow_dir)
+
+            season_directories = os.listdir(os.path.join(self.path, tvshow_dir))
+            for season_dir in season_directories:
+                season = TVShowSeason(season_dir)
+
+                episode_directories = os.listdir(os.path.join(self.path, tvshow_dir, season_dir))
+                for episode_dir in episode_directories:
+                    episode = TVShowEpisode(episode_dir)
+
+                    if episode.is_valid():
+                        season.add_episode(episode)
+                    else:
+                        self.invalid_episodes.append(f"{tvshow_dir} -> {season_dir} -> {episode_dir}")
+
+                if season.is_valid():
+                    tvshow.add_season(season)
+                else:
+                    self.invalid_seasons.append(f"{tvshow_dir} -> {season_dir}")
+
+            if tvshow.is_valid():
+                self.tvshowlist.add_tvshow(tvshow)
             else:
-                self.invalid_tvshows.append(directory)
+                self.invalid_tvshows.append(f"{tvshow_dir}")
 
-    def get_tvshows(self):
-        return self.tvshows
-
-    def get_seasons(self, tvshow_id):
-        tvshow = list(filter(lambda tvshow: tvshow["id"] == tvshow_id, self.tvshows))[0]
-        return tvshow["seasons"]
-
-    def get_episodes(self, tvshow_id, season_id):
-        tvshow = list(filter(lambda tvshow: tvshow["id"] == tvshow_id, self.tvshows))[0]
-        season = list(filter(lambda season: season["id"] == season_id, tvshow["seasons"]))[0]
-        return season["episodes"]
+    def get_tvshowlist(self):
+        return self.tvshowlist
 
     def get_invalid_tvshows(self):
         return self.invalid_tvshows
--- tests/shared/test_plex_tvshow_crawler.py
+++ tests/shared/test_plex_tvshow_crawler.py
@@ -46,29 +46,31 @@ class TestPlexTvshowsCrawler(unittest.TestCase):
         # initialize crawler
         self.crawler = PlexTvshowCrawler(tvshows_dir)
         self.crawler.crawl()
+        if self.crawler.get_tvshowlist().is_empty():
+            print("EMPTY TV SHOWS")
 
     def test_crawl_get_tvshows_count(self):
-        tvshows = self.crawler.get_tvshows()
+        tvshows = self.crawler.get_tvshowlist().get_tvshows()
         invalid_tvshows = self.crawler.get_invalid_tvshows()
         self.assertEqual(len(self.tvshow_directories), len(tvshows) + len(invalid_tvshows))
 
     def test_crawl_get_tvshows_object(self):
-        tvshows = self.crawler.get_tvshows()
-        codegeass = list(filter(lambda tvshow: tvshow["dirname"] == "Code Geass (2006) {tvdb-79525}", tvshows))[0]
-        self.assertEqual(codegeass["dirname"], "Code Geass (2006) {tvdb-79525}")
-        self.assertEqual(codegeass["id"], "79525")
+        tvshowlist = self.crawler.get_tvshowlist()
+        codegeass = tvshowlist.get_tvshow(79525)
+        self.assertEqual(codegeass.get_dirname(), "Code Geass (2006) {tvdb-79525}")
+        self.assertEqual(codegeass.get_tvdbid(), 79525)
 
     def test_crawl_get_invalid_tvshows(self):
         invalid_tvshows = self.crawler.get_invalid_tvshows()
         self.assertEqual("Classroom of the Elite (2017)", invalid_tvshows[0])
 
     def test_crawl_get_seasons_count(self):
-        seasons = self.crawler.get_seasons("79525")
+        seasons = self.crawler.get_tvshowlist().get_tvshow(79525).get_seasons()
         codegeass = list(filter(lambda tvshow: tvshow["dirname"] == "Code Geass (2006) {tvdb-79525}", self.tvshow_directories))[0]
         self.assertEqual(len(codegeass["seasons"]), len(seasons))
 
     def test_crawl_get_episodes_count(self):
-        episodes = self.crawler.get_episodes("79525", 1)
+        episodes = self.crawler.get_tvshowlist().get_tvshow(79525).get_season(1).get_episodes()
         codegeass = list(filter(lambda tvshow: tvshow["dirname"] == "Code Geass (2006) {tvdb-79525}", self.tvshow_directories))[0]
         season1 = list(filter(lambda season: season["dirname"] == "season 01", codegeass["seasons"]))[0]
         self.assertEqual(len(season1["episodes"]), len(episodes))
