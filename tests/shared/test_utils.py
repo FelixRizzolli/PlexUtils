72980bc9e729beb0d115c0b5a87a81ce0c062cbe
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-03 16:48:28 +0200

    *add test_extract_tvdbid

--- tests/shared/test_utils.py
+++ tests/shared/test_utils.py
@@ -1,3 +1,20 @@
+import unittest
+
+from utils import extract_tvdbid, extract_seasonid, extract_episodeid, extract_seasonid_from_episode
+
+class TestUtils(unittest.TestCase):
+    def test_extract_tvdbid(self):
+        got = extract_tvdbid("Game of Thrones (2011) {tvdb-121361}")
+        self.assertEqual(121361, got)
+
+        you_zitsu = extract_tvdbid("Classroom of the Elite (2017)")
+        self.assertIsNone(you_zitsu)
+
+        matrix = extract_tvdbid("The Matrix (1999) {tvdb-169}.mp4")
+        self.assertEqual(169, matrix)
+
+        baby_driver = extract_tvdbid("Baby Driver (2017).mp4")
+        self.assertIsNone(baby_driver)
 
     def test_extract_seasonid(self):
         pass
@@ -7,3 +24,7 @@
 
     def test_extract_seasonid_from_episode(self):
         pass
+
+
+if __name__ == '__main__':
+    unittest.main()
