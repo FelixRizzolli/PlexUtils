79a945ca590d5d41651ad87fd3f13f4523a5a65a
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-04 08:19:07 +0200

    add test_get_episodeids in TestTVDBTool

--- tests/shared/test_tvdb_tool.py
+++ tests/shared/test_tvdb_tool.py
@@ -40,6 +40,13 @@ class TestTVDBTool(unittest.TestCase):
 
         self.assertEqual(len(tvdb_got_seasons), len(plex_got_seasons))
 
+    def test_get_episodeids(self):
+        tvdb_tool = TVDBTool(self.config['tvdb-key'], self.config['tvdb-pin'])
+        tvdb_got_episodes = tvdb_tool.get_episodeids(121361, 1)
+        plex_got_episodes = self.crawler.get_tvshowlist().get_tvshow(121361).get_season(1).get_episodes()
+
+        self.assertEqual(len(tvdb_got_episodes), len(plex_got_episodes))
+
 
 if __name__ == '__main__':
     unittest.main()
