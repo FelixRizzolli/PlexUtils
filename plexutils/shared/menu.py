f433711b71cbd20ad10e5077dbe6523c46ecb478
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-04 19:27:21 +0200

    code refactoring

--- plexutils/shared/menu.py
+++ plexutils/shared/menu.py
@@ -8,20 +8,20 @@ def clear_console():
 
 
 class Menu(object):
-    def __init__(self, menu):
-        self.menu = menu
+    def __init__(self, menu_list):
+        self.menu_list = menu_list
 
     def id_exists(self, option_id):
-        for item in self.menu:
+        for item in self.menu_list:
             if item['id'] == option_id:
                 return True
         return False
 
     def get_option_by_id(self, option_id):
-        for option in self.menu:
+        for option in self.menu_list:
             if option['id'] == option_id:
                 return option
         return None
 
     def get_list(self):
-        return self.menu
+        return self.menu_list
