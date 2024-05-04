b29e9e5925081a95a37a78648d4ff76f307cf2d8
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-04 07:00:59 +0200

    code refactoring

--- tests/shared/test_tvdb_tool.py
+++ tests/shared/test_tvdb_tool.py
@@ -6,7 +6,7 @@ import yaml
 from plex_tvshow_crawler import PlexTvshowCrawler
 from test_data import test_tvshow_files
 
-from utils import delete_directory, load_config
+from utils import load_config
 from tvdb_tool import TVDBTool
 
 current_script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -19,32 +19,6 @@ class TestTVDBTool(unittest.TestCase):
     def setUp(self):
         self.tvshow_directories = test_tvshow_files
         self.config = load_config(os.path.join(current_script_dir, '../../config.yaml'))
-
-        # clear data
-        if os.path.isdir(tvshows_dir):
-            delete_directory(tvshows_dir)
-
-        # create data
-        if not os.path.isdir(data_dir):
-            os.mkdir(data_dir)
-        if not os.path.isdir(tvshows_dir):
-            os.mkdir(tvshows_dir)
-
-        for tvshow in self.tvshow_directories:
-            tvshow_dirname = tvshow["dirname"]
-            tvshow_dir = os.path.join(tvshows_dir, tvshow_dirname)
-            if not os.path.isdir(tvshow_dir):
-                os.mkdir(tvshow_dir)
-
-            for season in tvshow["seasons"]:
-                season_dirname = season["dirname"]
-                season_dir = os.path.join(tvshow_dir, season_dirname)
-                if not os.path.isdir(season_dir):
-                    os.mkdir(season_dir)
-
-                for episode in season["episodes"]:
-                    episode_dir = os.path.join(season_dir, episode)
-                    open(episode_dir, 'a').close()
 
         # initialize crawler
         self.crawler = PlexTvshowCrawler(tvshows_dir)
