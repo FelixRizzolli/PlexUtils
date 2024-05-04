96a1e8bbf1080702f3528121be7a0cd6bfd8b8fa
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-04 20:11:37 +0200

    code refactoring

--- tests/shared/test_tvdb_tool.py
+++ tests/shared/test_tvdb_tool.py
@@ -27,21 +27,29 @@ class TestTVDBTool(unittest.TestCase):
     def test_get_episodes(self):
         tvdb_tool = TVDBTool(self.config['tvdb-key'], self.config['tvdb-pin'])
         tvdb_got_episodes = tvdb_tool.get_episodes(121361, 1)
-        plex_got_episodes = self.crawler.get_tvshowlist().get_tvshow(121361).get_season(1).get_episodes()
+        plex_got_episodes = (self.crawler.get_tvshowlist()
+                             .get_tvshow(121361)
+                             .get_season(1)
+                             .get_episodes())
 
         self.assertEqual(len(tvdb_got_episodes), len(plex_got_episodes))
 
     def test_get_seasons(self):
         tvdb_tool = TVDBTool(self.config['tvdb-key'], self.config['tvdb-pin'])
         tvdb_got_seasons = tvdb_tool.get_seasonids(121361)
-        plex_got_seasons = self.crawler.get_tvshowlist().get_tvshow(121361).get_seasons()
+        plex_got_seasons = (self.crawler.get_tvshowlist()
+                            .get_tvshow(121361)
+                            .get_seasons())
 
         self.assertEqual(len(tvdb_got_seasons), len(plex_got_seasons))
 
     def test_get_episodeids(self):
         tvdb_tool = TVDBTool(self.config['tvdb-key'], self.config['tvdb-pin'])
         tvdb_got_episodes = tvdb_tool.get_episodeids(121361, 1)
-        plex_got_episodes = self.crawler.get_tvshowlist().get_tvshow(121361).get_season(1).get_episodes()
+        plex_got_episodes = (self.crawler.get_tvshowlist()
+                             .get_tvshow(121361)
+                             .get_season(1)
+                             .get_episodes())
 
         self.assertEqual(len(tvdb_got_episodes), len(plex_got_episodes))
 
