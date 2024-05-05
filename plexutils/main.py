28511d8e3d384d0c5016a2d05045aa674a21426a
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-05 09:00:18 +0200

    add type hints

--- plexutils/main.py
+++ plexutils/main.py
@@ -2,17 +2,18 @@
     This module contains the main function.
 """
 import os
+from typing import Callable
 
 from plexutils.utils.plex_utils import PlexUtils
 from plexutils.shared.utils import load_config, setup_i18n
 
 
 if __name__ == '__main__':
-    pj_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    config_dir = os.path.join(pj_path + '/config.yaml')
-    config = load_config(config_dir)
+    pj_path: str = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    config_dir: str = os.path.join(pj_path + '/config.yaml')
+    config: dict = load_config(config_dir)
 
-    gettext = setup_i18n(pj_path, config)
+    gettext: Callable[[str], str] = setup_i18n(pj_path, config)
 
-    plex_utils = PlexUtils(config, gettext)
+    plex_utils: PlexUtils = PlexUtils(config, gettext)
     plex_utils.print_menu()
