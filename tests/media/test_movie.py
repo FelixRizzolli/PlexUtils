cd0f9323916a1889ce86198edaa4a4b80bb85e60
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-06 19:02:54 +0200

    add TestMovie - test_property_tvdbid

--- tests/media/test_movie.py
+++ tests/media/test_movie.py
@@ -3,9 +3,16 @@
 """
 import unittest
 
+from plexutils.media.movie import Movie
+
 
 class TestMovie(unittest.TestCase):
     """test class for the Movie class"""
+
+    def test_property_tvdbid(self) -> None:
+        """tests the tvdbid property of the Movie class"""
+        movie: Movie = Movie("Happy Death Day (2017) {tvdb-475}.mp4")
+        self.assertEqual(1, movie.tvdbid)
 
 
 if __name__ == '__main__':
