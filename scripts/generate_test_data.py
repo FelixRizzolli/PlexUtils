68937afdcc40135efb8969156e539331a3f60c2a
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-07 06:34:25 +0200

    bugfix

--- scripts/generate_test_data.py
+++ scripts/generate_test_data.py
@@ -17,10 +17,12 @@ def delete_directory(dir_path):
     """
     for item in os.listdir(dir_path):
         item_path: str = os.path.join(dir_path, item)
+
         if os.path.isfile(item_path):
             os.remove(item_path)
         elif os.path.isdir(item_path):
             delete_directory(item_path)
+
     os.rmdir(dir_path)
 
 
@@ -40,10 +42,10 @@ def generate_test_data():
     # Define the path to the 'scripts/data' directory
     scripts_data_dir: str = os.path.join(current_script_dir, 'data')
 
-    if os.path.isdir(data_dir):
-        delete_directory(os.path.join(data_dir, 'movies'))
-    generate_movie_files(data_dir, scripts_data_dir)
+    # Delete the 'data' directory
+    if os.path.exists(data_dir):
+        delete_directory(os.path.join(data_dir))
 
-    if os.path.isdir(data_dir):
-        delete_directory(os.path.join(data_dir, 'tvshows'))
+    # Generate the data
+    generate_movie_files(data_dir, scripts_data_dir)
     generate_tvshow_directories(data_dir, scripts_data_dir)
