7c977651e9fd6189e197f08098ed9494d2f15149
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-08 19:10:07 +0200

    add test_property_filename to TestTVShowEpisode

--- tests/media/test_tvshow_episode.py
+++ tests/media/test_tvshow_episode.py
@@ -12,9 +12,20 @@ class TestTVShowEpisode(unittest.TestCase):
     def test_property_episode_id(self) -> None:
         """tests the episode_id property of the TVShowEpisode class"""
         episode: TVShowEpisode = TVShowEpisode(
-            "Code Geass (2006) - s01e01 - The Day a New Demon Was Born"
+            "Code Geass (2006) - s01e01 - The Day a New Demon Was Born.mp4"
         )
         self.assertEqual(1, episode.episode_id)
+
+    def test_property_filename(self) -> None:
+        """tests the filename property of the TVShowEpisode class"""
+        episode: TVShowEpisode = TVShowEpisode(
+            "Code Geass (2006) - s01e01 - The Day a New Demon Was Born.mp4"
+        )
+        self.assertEqual(
+            "Code Geass (2006) - s01e01 - The Day a New Demon Was Born.mp4",
+            episode.filename
+        )
+
 
 
 if __name__ == '__main__':
