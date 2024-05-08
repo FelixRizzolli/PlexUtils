14e96e0040593ffd20d377cffa47a7acbf88f3f4
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-08 19:57:07 +0200

    add test_property_tvshows to TestTVShowList

--- tests/media/test_tvshow_list.py
+++ tests/media/test_tvshow_list.py
@@ -10,6 +10,15 @@ from plexutils.media.tvshow import TVShow
 class TestTVShowList(unittest.TestCase):
     """test class for the TVShowList class"""
 
+    def test_property_tvshows(self) -> None:
+        """tests the tvshows property of the TVShowList class"""
+        tvshowlist: TVShowList = TVShowList()
+        self.assertEqual(0, len(tvshowlist.tvshows))
+        tvshowlist.add_tvshow(TVShow("Code Geass (2006) {tvdb-79525}"))
+        self.assertEqual(1, len(tvshowlist.tvshows))
+        tvshowlist.add_tvshow(TVShow("Game of Thrones (2011) {tvdb-121361}"))
+        self.assertEqual(2, len(tvshowlist.tvshows))
+
     def test_is_empty(self) -> None:
         """tests the is_empty method of the TVShowList class"""
         tvshowlist: TVShowList = TVShowList()
