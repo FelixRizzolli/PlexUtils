139cbd0dbfa4168674d59ab7147c775f15f9e619
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-08 19:33:15 +0200

    add test_property_dirname to TestTVShowSeason

--- tests/media/test_tvshow_season.py
+++ tests/media/test_tvshow_season.py
@@ -4,6 +4,7 @@
 import unittest
 
 from plexutils.media.tvshow_season import TVShowSeason
+from plexutils.media.tvshow_episode import TVShowEpisode
 
 
 class TestTVShowSeason(unittest.TestCase):
@@ -13,6 +14,11 @@ class TestTVShowSeason(unittest.TestCase):
         """tests the season_id property of the TVShowSeason class"""
         season: TVShowSeason = TVShowSeason("season 01")
         self.assertEqual(1, season.season_id)
+
+    def test_property_dirname(self) -> None:
+        """tests the dirname property of the TVShowSeason class"""
+        season: TVShowSeason = TVShowSeason("season 01")
+        self.assertEqual("season 01", season.dirname)
 
 
 if __name__ == '__main__':
