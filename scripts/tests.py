086f4ba9ee9e089a6d93330e1636db04eec90e04
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-19 20:57:55 +0200

    code refactoring

--- scripts/tests.py
+++ scripts/tests.py
@@ -1,4 +1,26 @@
+"""
+This module provides functions to run unit tests and generate coverage reports.
+
+It includes functions to run unit tests in the 'tests' directory using Python's built-in
+unittest module. It also provides functions to generate coverage reports in multiple
+formats (HTML, XML, LCOV, JSON) using the Coverage.py library.
+
+Functions:
+    run_tests: Runs unit tests in the 'tests' directory.
+    run_test_coverage: Executes test coverage analysis for the project and generates
+                       detailed reports.
+    create_html_report: Creates an HTML coverage report.
+    create_xml_report: Creates an XML coverage report.
+    create_lcov_report: Creates an LCOV coverage report.
+    create_json_report: Creates a JSON coverage report.
+"""
+
+
+import os
 import subprocess
+import unittest
+
+from coverage import Coverage
 
 
 def run_tests() -> None:
@@ -11,3 +33,95 @@ def run_tests() -> None:
     """
     subprocess.run("poetry run python -m unittest discover tests", shell=True, check=True)
 
+
+def run_test_coverage():
+    """
+    Execute test coverage analysis for the project.
+
+    This function starts the coverage measurement, discovers all tests in the 'tests' directory,
+    runs them, and then stops the coverage. It reports the coverage data to the console and
+    generates detailed reports in multiple formats including HTML, XML, LCOV, and JSON.
+
+    Reports:
+        - Console: Displays a summary of the coverage analysis.
+        - HTML: Generates a directory 'htmlcov' with an interactive HTML report.
+        - XML: Creates a file 'coverage.xml' with the coverage report in XML format.
+        - LCOV: Generates an LCOV report file 'lcov.info'.
+        - JSON: Creates a JSON report file 'coverage.json'.
+    """
+    # Get the absolute path of the current script
+    current_script_dir: str = os.path.dirname(os.path.realpath(__file__))
+
+    # Start the coverage measurement
+    sources_dir = os.path.join(current_script_dir, "../plexutils")
+    cov = Coverage(source=[sources_dir])
+    cov.start()
+
+    # Discover and run tests
+    tests_dir = os.path.join(current_script_dir, "../tests")
+    loader = unittest.TestLoader()
+    suite = loader.discover(tests_dir)
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
+    # Stop the coverage measurement and save
+    cov.stop()
+    cov.save()
+
+    # Report the results to the console
+    cov.report()
+
+    # Generate detailed reports
+    report_dir: str = os.path.join(current_script_dir, "..", "reports", "coverage")
+    create_html_report(cov, report_dir)
+    create_xml_report(cov, report_dir)
+    create_lcov_report(cov, report_dir)
+    create_json_report(cov, report_dir)
+
+
+def create_html_report(cov: Coverage, report_dir: str) -> None:
+    """
+    Creates an HTML coverage report.
+
+    Args:
+        cov (Coverage): The coverage object.
+        report_dir (str): The directory where the report will be saved.
+    """
+    html_report_dir: str = os.path.join(report_dir, "html")
+    cov.html_report(directory=html_report_dir)
+
+
+def create_xml_report(cov: Coverage, report_dir: str) -> None:
+    """
+    Creates an XML coverage report.
+
+    Args:
+        cov (Coverage): The coverage object.
+        report_dir (str): The directory where the report will be saved.
+    """
+    xml_report_dir: str = os.path.join(report_dir, "xml", "coverage.xml")
+    cov.xml_report(outfile=xml_report_dir)
+
+
+def create_lcov_report(cov: Coverage, report_dir: str) -> None:
+    """
+    Creates an LCOV coverage report.
+
+    Args:
+        cov (Coverage): The coverage object.
+        report_dir (str): The directory where the report will be saved.
+    """
+    lcov_report_dir: str = os.path.join(report_dir, "lcov", "lcov.info")
+    cov.lcov_report(outfile=lcov_report_dir)
+
+
+def create_json_report(cov: Coverage, report_dir: str) -> None:
+    """
+    Creates a JSON coverage report.
+
+    Args:
+        cov (Coverage): The coverage object.
+        report_dir (str): The directory where the report will be saved.
+    """
+    json_report_dir: str = os.path.join(report_dir, "json", "coverage.json")
+    cov.json_report(outfile=json_report_dir)
