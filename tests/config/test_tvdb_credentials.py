0c9ae651cf0594e4d81d5a33b068bec86ffb9e70
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-21 06:31:51 +0200

    add TVDBCredentials

---
+++ tests/config/test_tvdb_credentials.py
@@ -0,0 +1,45 @@
+"""
+This module contains the TestTVDBCredentials class which is used to test the
+TVDBCredentials class.
+"""
+
+import unittest
+
+from plexutils.config.tvdb_credentials import TVDBCredentials
+
+
+class TestTVDBCredentials(unittest.TestCase):
+    """
+    Test case for the TVDBCredentials class.
+    """
+
+    def setUp(self):
+        """
+        Set up the test case.
+        """
+        self.api_key = "test_api_key"
+        self.api_pin = "test_api_pin"
+        self.credentials = TVDBCredentials(self.api_key, self.api_pin)
+
+    def test_initialization(self):
+        """
+        Test the initialization of the TVDBCredentials class.
+        """
+        self.assertEqual(self.credentials.api_key, self.api_key)
+        self.assertEqual(self.credentials.api_pin, self.api_pin)
+
+    def test_api_key_property(self):
+        """
+        Test the api_key property of the TVDBCredentials class.
+        """
+        self.assertEqual(self.credentials.api_key, self.api_key)
+
+    def test_api_pin_property(self):
+        """
+        Test the api_pin property of the TVDBCredentials class.
+        """
+        self.assertEqual(self.credentials.api_pin, self.api_pin)
+
+
+if __name__ == "__main__":
+    unittest.main()
