b2091a817ee45d05b9d37b6005487f8d1dc44364
Felix Rizzolli <felix.rizzolli@outlook.de>
2024-05-23 05:36:50 +0200

    code refactoring

--- plexutils/config/tvdb_credentials.py
+++ plexutils/config/tvdb_credentials.py
@@ -3,39 +3,13 @@ This module contains the TVDBCredentials class which is used to store and retrie
 TVDB API credentials.
 """
 
+from dataclasses import dataclass
 
+
+@dataclass
 class TVDBCredentials:
     """
     A class used to represent TVDB API credentials.
     """
-
-    def __init__(self, api_key: str, api_pin: str):
-        """
-        Constructs all the necessary attributes for the TVDBCredentials object.
-
-        Args:
-            api_key(str): The TVDB API key.
-            api_pin(str): The TVDB API pin.
-        """
-        self._api_key: str = api_key
-        self._api_pin: str = api_pin
-
-    @property
-    def api_key(self) -> str:
-        """
-        Returns the TVDB API key.
-
-        Returns:
-            str: The TVDB API key.
-        """
-        return self._api_key
-
-    @property
-    def api_pin(self) -> str:
-        """
-        Returns the TVDB API pin.
-
-        Returns:
-            str: The TVDB API pin.
-        """
-        return self._api_pin
+    api_key: str
+    api_pin: str
